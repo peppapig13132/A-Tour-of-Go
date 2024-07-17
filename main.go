c1dfc40afdb7ce4920428caec0c6e8bcc57079cf
peppapig13132 <peppapig13132@outlook.com>
2024-07-17 15:24:29 +0200

    Short variable declarations

--- main.go
+++ main.go
@@ -2,9 +2,10 @@ package main
 
 import "fmt"
 
-var i, j int = 1, 2
-
 func main() {
-	var c, python, java = true, false, "no!"
-	fmt.Println(i, j, c, python, java)
+	var i, j int = 1, 2
+	k := 3
+	c, python, java := true, false, "no!"
+
+	fmt.Println(i, j, k, c, python, java)
 }
