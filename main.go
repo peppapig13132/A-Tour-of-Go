8ae2e244af036aa20916b36eb77284d041bb4790
peppapig13132 <peppapig13132@outlook.com>
2024-07-17 08:33:19 +0200

    Multiple results

--- main.go
+++ main.go
@@ -4,10 +4,11 @@ import (
 	"fmt"
 )
 
-func add(x, y int) int {
-	return x + y
+func swap(x, y string) (string, string) {
+	return y, x
 }
 
 func main() {
-	fmt.Println(add(42, 13))
+	a, b := swap("hello", "world")
+	fmt.Println(a, b)
 }
