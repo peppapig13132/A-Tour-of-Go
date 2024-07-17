5e1d96eea0566b3e85277de1b466a2b48b8193a3
peppapig13132 <peppapig13132@outlook.com>
2024-07-17 15:44:41 +0200

    Type conversions

--- main.go
+++ main.go
@@ -1,11 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 func main() {
-	var i int
-	var f float64
-	var b bool
-	var s string
-	fmt.Printf("%v %v %v %q\n", i, f, b, s)
+	var x, y int = 3, 4
+	var f float64 = math.Sqrt(float64(x*x + y*y))
+	var z uint = uint(f)
+	fmt.Println(x, y, z)
 }
