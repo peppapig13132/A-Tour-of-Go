8c797c4799cbd47ec193c7844dd33c7d5b53ed8e
peppapig13132 <peppapig13132@outlook.com>
2024-07-17 08:36:10 +0200

    Named return values

--- main.go
+++ main.go
@@ -4,11 +4,12 @@ import (
 	"fmt"
 )
 
-func swap(x, y string) (string, string) {
-	return y, x
+func split(sum int) (x, y int) {
+	x = sum * 4 / 9
+	y = sum - x
+	return
 }
 
 func main() {
-	a, b := swap("hello", "world")
-	fmt.Println(a, b)
+	fmt.Println(split(17))
 }
