7de89b72014157c3bff77be8b8804e1ca552c967
peppapig13132 <peppapig13132@outlook.com>
2024-07-17 15:38:22 +0200

    Basic types

--- main.go
+++ main.go
@@ -1,11 +1,38 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/cmplx"
+)
+
+var (
+	ToBe		bool				= false
+	MaxInt 	uint64			= 1<<64 - 1
+	z				complex128	= cmplx.Sqrt(-5 + 12i)
+)
 
 func main() {
-	var i, j int = 1, 2
-	k := 3
-	c, python, java := true, false, "no!"
-
-	fmt.Println(i, j, k, c, python, java)
+	fmt.Printf("Type: %T Value: %v\n", ToBe, ToBe)
+	fmt.Printf("Type: %T Value: %v\n", MaxInt, MaxInt)
+	fmt.Printf("Type: %T Value: %v\n", z, z)
 }
+
+/*
+# Go's basic types are:
+
+bool
+
+string
+
+int  int8  int16  int32  int64
+uint uint8 uint16 uint32 uint64 uintptr
+
+byte // alias for uint8
+
+rune // alias for int32
+     // represents a Unicode code point
+
+float32 float64
+
+complex64 complex128
+*/
