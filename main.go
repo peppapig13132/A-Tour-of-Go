6be31337242b314edd4a5ded9a174641bd000ae7
peppapig13132 <peppapig13132@outlook.com>
2024-07-17 15:21:17 +0200

    Variables with initializers

--- main.go
+++ main.go
@@ -1,12 +1,10 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
-var c, python, java bool
+var i, j int = 1, 2
 
 func main() {
-	var i int
-	fmt.Println(i, c, python, java)
+	var c, python, java = true, false, "no!"
+	fmt.Println(i, j, c, python, java)
 }
