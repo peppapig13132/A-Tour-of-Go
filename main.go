b57a4b0570c86d53ffd968bc5e45e1f46c4df07b
peppapig13132 <peppapig13132@outlook.com>
2024-07-17 08:30:16 +0200

    Functions

--- main.go
+++ main.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"fmt"
-	"math"
 )
 
+func add(x int, y int) int {
+	return x + y
+}
+
 func main() {
-	// fmt.Println(math.pi)
-	fmt.Println(math.Pi)
+	fmt.Println(add(42, 13))
 }
