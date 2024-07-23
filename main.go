dabc860fea7590823e43172b9d38d29da5c86ce5
peppapig13132 <peppapig13132@outlook.com>
2024-07-23 04:18:52 +0200

    Switch evaluation order

--- main.go
+++ main.go
@@ -2,19 +2,21 @@ package main
 
 import (
 	"fmt"
-	"runtime"
+	"time"
 )
 
 func main() {
-	fmt.Print("Go runs on ")
-	switch os := runtime.GOOS; os {
-	case "darwin":
-		fmt.Println("OS X.")
-	case "linux":
-		fmt.Println("Linux.")
+	fmt.Println("When's Saturday?")
+	today := time.Now().Weekday()
+	// fmt.Println(time.Saturday == 6)
+	switch time.Saturday {
+	case today + 0:
+		fmt.Println("Today.")
+	case today + 1:
+		fmt.Println("Tommorow.")
+	case today + 2:
+		fmt.Println("In two days.")
 	default:
-		// freebsd, openbsd,
-		// plan9, windows...
-		fmt.Printf("%s.\n", os)
+		fmt.Println("Too far away.")
 	}
 }
