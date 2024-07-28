52643878b924f6b96c5d61dbb45977ab6b93ea99
peppapig13132 <peppapig13132@outlook.com>
2024-07-28 20:06:20 +0200

    Structs

--- main.go
+++ main.go
@@ -4,15 +4,11 @@ import (
 	"fmt"
 )
 
+type Vertex struct {
+	X int
+	Y int
+}
+
 func main() {
-	i, j := 42, 2701
-
-	p := &i							// point to i
-	fmt.Println(*p)			// read i through the pointer
-	*p = 21							// set i through the pointer
-	fmt.Println(i)			// see the new value of i
-
-	p = &j							// point to j
-	*p = *p / 37 				// divide j through the pointer
-	fmt.Println(j)			// see the new value of j
+	fmt.Println(Vertex{1, 2})
 }
