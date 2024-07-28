f03d6a7cbc6e398fb3241c85112f7ef3a7fe9443
peppapig13132 <peppapig13132@outlook.com>
2024-07-28 20:42:03 +0200

    Arrays

--- main.go
+++ main.go
@@ -4,18 +4,13 @@ import (
 	"fmt"
 )
 
-type Vertex struct {
-	X, Y int
-}
-
-var (
-	v1 = Vertex{1, 2}
-	v2 = Vertex{X: 1}
-	v3 = Vertex{}
-	p = &Vertex{1, 2}
-)
-
 func main() {
-	fmt.Println(v1, p, v2, v3)
-	fmt.Printf("%p\n", p)
+	var a [2] string
+	a[0] = "Hello"
+	a[1] = "World"
+	fmt.Println(a[0], a[1])
+	fmt.Println(a)
+
+	primes := [6]int{2, 3, 5, 7, 11, 13}
+	fmt.Println(primes)
 }
