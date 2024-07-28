be844af3897506928ebbb355168500286529b8a3
peppapig13132 <peppapig13132@outlook.com>
2024-07-28 20:37:47 +0200

    Struct Literals

--- main.go
+++ main.go
@@ -5,13 +5,17 @@ import (
 )
 
 type Vertex struct {
-	X int
-	Y int
+	X, Y int
 }
 
+var (
+	v1 = Vertex{1, 2}
+	v2 = Vertex{X: 1}
+	v3 = Vertex{}
+	p = &Vertex{1, 2}
+)
+
 func main() {
-	v := Vertex{1, 2}
-	p := &v
-	p.X = 1e9
-	fmt.Println(v)
+	fmt.Println(v1, p, v2, v3)
+	fmt.Printf("%p\n", p)
 }
