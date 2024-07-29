80ff1663535b62f4fd41db21f9564987185e9c7d
peppapig13132 <peppapig13132@outlook.com>
2024-07-29 06:30:52 +0200

    Slices are like references to arrays

--- main.go
+++ main.go
@@ -3,8 +3,19 @@ package main
 import "fmt"
 
 func main() {
-	primes := [6]int{2, 3, 5, 7, 11, 13}
+	names := [4]string{
+		"John",
+		"Paul",
+		"George",
+		"Ringo",
+	}
+	fmt.Println(names)
 
-	var s []int = primes[1:4]
-	fmt.Println(s)
+	a := names[0:2]
+	b := names[1:3]
+	fmt.Println(a, b)
+
+	b[0] = "XXX"
+	fmt.Println(a, b)
+	fmt.Println(names)
 }
