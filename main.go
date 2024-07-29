e5c4770038911d51e65f934b17406f5b1fe16140
peppapig13132 <peppapig13132@outlook.com>
2024-07-29 06:43:26 +0200

    Nil slices

--- main.go
+++ main.go
@@ -3,14 +3,9 @@ package main
 import "fmt"
 
 func main() {
-	s := []int{2, 3, 5, 7, 11, 13}
-
-	s = s[1:4]
-	fmt.Println(s)
-
-	s = s[:2]
-	fmt.Println(s)
-
-	s = s[1:]
-	fmt.Println(s)
+	var s []int
+	fmt.Println(s, len(s), cap(s))
+	if s == nil {
+		fmt.Println("nil!")
+	}
 }
