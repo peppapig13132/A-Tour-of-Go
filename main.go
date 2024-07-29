892207ac89c618499adca63054dd83310a6774c0
peppapig13132 <peppapig13132@outlook.com>
2024-07-29 06:34:31 +0200

    Slice literals

--- main.go
+++ main.go
@@ -3,19 +3,22 @@ package main
 import "fmt"
 
 func main() {
-	names := [4]string{
-		"John",
-		"Paul",
-		"George",
-		"Ringo",
+	q := []int{2, 3, 5, 7, 11, 13}
+	fmt.Println(q)
+
+	r := []bool{true, false, true, true, false, true}
+	fmt.Println(r)
+
+	s := []struct {
+		i int
+		b bool
+	}{
+		{2, true},
+		{3, false},
+		{5, true},
+		{7, true},
+		{11, false},
+		{13, true},
 	}
-	fmt.Println(names)
-
-	a := names[0:2]
-	b := names[1:3]
-	fmt.Println(a, b)
-
-	b[0] = "XXX"
-	fmt.Println(a, b)
-	fmt.Println(names)
+	fmt.Println(s)
 }
