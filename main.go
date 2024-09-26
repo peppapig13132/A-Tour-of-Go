83990280db72b8062ed8c4a9c3cddba5342a7355
peppapig13132 <peppapig13132@outlook.com>
2024-09-26 15:48:08 +0200

    Function values

--- main.go
+++ main.go
@@ -1,19 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
+
+func compute(fn func(float64, float64) float64) float64 {
+	return fn(3, 4)
+}
 
 func main() {
-	m := make(map[string]int)
+	hypot := func(x, y float64) float64 {
+		return math.Sqrt(x*x + y*y)
+	}
+	fmt.Println(hypot(5, 12))
 
-	m["Answer"] = 42
-	fmt.Println("The value:", m["Answer"])
-
-	m["Answer"] = 48
-	fmt.Println("The value:", m["Answer"])
-
-	delete(m, "Answer")
-	fmt.Println("The value:", m["Answer"])
-
-	v, ok := m["Answer"]
-	fmt.Println("The value:", v, "Present?", ok)
+	fmt.Println(compute(hypot))
+	fmt.Println(compute(math.Pow))
 }
