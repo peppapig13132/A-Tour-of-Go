bc4ccc86b16de140e50d3763e84cea1917a7c9e6
peppapig13132 <peppapig13132@outlook.com>
2024-09-26 15:54:30 +0200

    Function closures

--- main.go
+++ main.go
@@ -1,20 +1,21 @@
 package main
 
-import (
-	"fmt"
-	"math"
-)
+import "fmt"
 
-func compute(fn func(float64, float64) float64) float64 {
-	return fn(3, 4)
+func adder() func(int) int {
+	sum := 0
+	return func(x int) int {
+		sum += x
+		return sum
+	}
 }
 
 func main() {
-	hypot := func(x, y float64) float64 {
-		return math.Sqrt(x*x + y*y)
+	pos, neg := adder(), adder()
+	for i := 0; i < 10; i++ {
+		fmt.Println(
+			pos(i),
+			neg(-2*i),
+		)
 	}
-	fmt.Println(hypot(5, 12))
-
-	fmt.Println(compute(hypot))
-	fmt.Println(compute(math.Pow))
 }
