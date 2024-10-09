4c05847e5567be60fbeaf9e5eb23e80b545246fc
peppapig13132 <peppapig13132@outlook.com>
2024-10-09 13:08:23 +0200

    Interfaces are implemented implicitly

--- main.go
+++ main.go
@@ -1,41 +1,22 @@
 package main
 
-import (
-	"fmt"
-	"math"
-)
-type Abser interface {
-	Abs() float64
+import "fmt"
+
+type I interface {
+	M()
+}
+
+type T struct {
+	S string
+}
+
+// This method means type T implements the interface I,
+// but we don't need to explicitly declare that it does so.
+func (t T) M() {
+	fmt.Println(t.S)
 }
 
 func main() {
-	var a Abser
-	f := MyFloat(-math.Sqrt2)
-	v := Vertex{3, 4}
-
-	a = f  // a MyFloat implements Abser
-	a = &v // a *Vertex implements Abser
-
-	// In the following line, v is an Vertex (not *Vertex)
-	// and does NOT implement Abser
-	a = v
-
-	fmt.Println(a.Abs())
-}
-
-type MyFloat float64
-
-func (f MyFloat) Abs() float64 {
-	if f < 0 {
-		return float64(-f)
-	}
-	return float64(f)
-}
-
-type Vertex struct {
-	X, Y float64
-}
-
-func (v *Vertex) Abs() float64 {
-	return math.Sqrt(v.X*v.X + v.Y*v.Y)
+	var i I = T{"hello"}
+	i.M()
 }
