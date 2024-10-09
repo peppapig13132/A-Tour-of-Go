733b170534e173b7aece626284fad283cb7a5742
peppapig13132 <peppapig13132@outlook.com>
2024-10-09 23:00:12 +0200

    Readers

--- main.go
+++ main.go
@@ -2,27 +2,20 @@ package main
 
 import (
 	"fmt"
-	"time"
+	"io"
+	"strings"
 )
 
-type MyError struct {
-	When time.Time
-	What string
-}
-
-func (e *MyError) Error() string {
-	return fmt.Sprintf("at %v, %s", e.When, e.What)
-}
-
-func run() error {
-	return &MyError{
-		time.Now(),
-		"it didn't work",
-	}
-}
-
 func main() {
-	if err := run(); err != nil {
-		fmt.Println(err)
+	r := strings.NewReader("Hello, Reader!")
+
+	b := make([]byte, 8)
+	for {
+		n, err := r.Read(b)
+		fmt.Printf("n = %v err = %v b = %v\n", n, err, b)
+		fmt.Printf("b[:n] = %q\n", b[:n])
+		if err == io.EOF {
+			break
+		}
 	}
 }
