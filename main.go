65a2f66f02e5e0bf13d4455518098f81fb9acb2c
peppapig13132 <peppapig13132@outlook.com>
2024-10-09 09:44:33 +0200

    Pointer receivers

--- main.go
+++ main.go
@@ -5,16 +5,21 @@ import (
 	"math"
 )
 
-type MyFloat float64
+type Vertex struct {
+	X, Y float64
+}
 
-func (f MyFloat) Abs() float64 {
-	if f < 0 {
-		return float64(-f)
-	}
-	return float64(f)
+func (v Vertex) Abs() float64 {
+	return math.Sqrt(v.X*v.X + v.Y*v.Y)
+}
+
+func (v *Vertex) Scale(f float64) {
+	v.X = v.X * f
+	v.Y = v.Y * f
 }
 
 func main() {
-	f := MyFloat(-math.Sqrt2)
-	fmt.Println(f.Abs())
+	v := Vertex{3, 4}
+	v.Scale(10)
+	fmt.Println(v.Abs())
 }
