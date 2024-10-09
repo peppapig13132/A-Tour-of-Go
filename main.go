7fcda7d45095005028a1ae17bf203de9678533d0
peppapig13132 <peppapig13132@outlook.com>
2024-10-09 04:19:19 +0200

    Methods continued

--- main.go
+++ main.go
@@ -5,15 +5,16 @@ import (
 	"math"
 )
 
-type Vertex struct {
-	X, Y float64
-}
+type MyFloat float64
 
-func Abs(v Vertex) float64 {
-	return math.Sqrt(v.X*v.X + v.Y*v.Y)
+func (f MyFloat) Abs() float64 {
+	if f < 0 {
+		return float64(-f)
+	}
+	return float64(f)
 }
 
 func main() {
-	v := Vertex{3, 4}
-	fmt.Println(Abs(v))
+	f := MyFloat(-math.Sqrt2)
+	fmt.Println(f.Abs())
 }
