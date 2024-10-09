28a4a525c2d0343dae2d40871ac4f10e6b6585c5
peppapig13132 <peppapig13132@outlook.com>
2024-10-09 09:47:09 +0200

    Pointers and functions

--- main.go
+++ main.go
@@ -9,17 +9,17 @@ type Vertex struct {
 	X, Y float64
 }
 
-func (v Vertex) Abs() float64 {
+func Abs(v Vertex) float64 {
 	return math.Sqrt(v.X*v.X + v.Y*v.Y)
 }
 
-func (v *Vertex) Scale(f float64) {
+func Scale(v *Vertex, f float64) {
 	v.X = v.X * f
 	v.Y = v.Y * f
 }
 
 func main() {
 	v := Vertex{3, 4}
-	v.Scale(10)
-	fmt.Println(v.Abs())
+	Scale(&v, 10)
+	fmt.Println(v)
 }
