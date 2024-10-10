f0e3e3c1764b68d262a4c4d3515367176b8b0c48
peppapig13132 <peppapig13132@outlook.com>
2024-10-10 19:24:08 +0200

    Images

--- main.go
+++ main.go
@@ -2,20 +2,11 @@ package main
 
 import (
 	"fmt"
-	"io"
-	"strings"
+	"image"
 )
 
 func main() {
-	r := strings.NewReader("Hello, Reader!")
-
-	b := make([]byte, 8)
-	for {
-		n, err := r.Read(b)
-		fmt.Printf("n = %v err = %v b = %v\n", n, err, b)
-		fmt.Printf("b[:n] = %q\n", b[:n])
-		if err == io.EOF {
-			break
-		}
-	}
+	m := image.NewRGBA(image.Rect(0, 0, 100, 100))
+	fmt.Println(m.Bounds())
+	fmt.Println(m.At(0, 0).RGBA())
 }
