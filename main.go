a52576a6ca966ed958749d8843d3146df1fd22a7
peppapig13132 <peppapig13132@outlook.com>
2024-10-12 02:53:58 +0200

    Channels

--- main.go
+++ main.go
@@ -1,18 +1,25 @@
 package main
 
-import (
-	"fmt"
-	"time"
-)
+import "fmt"
 
-func say(s string) {
-	for i := 0; i < 5; i++ {
-		time.Sleep(1 * time.Millisecond)
-		fmt.Println(s)
+func sum(s []int, c chan int) {
+	fmt.Println(s)
+	sum := 0
+	for _, v := range s {
+		sum += v
 	}
+	c <- sum	// send sum to c
+
+	fmt.Println(c)
 }
 
 func main() {
-	go say("world")
-	say("hello")
+	s := []int{7, 2, 8, -9, 4, 0}
+
+	c := make(chan int)
+	go sum(s[:len(s)/2], c)
+	go sum(s[len(s)/2:], c)
+	x, y := <-c, <-c	// receive from c
+
+	fmt.Println(x, y, x+y)
 }
