c48f9f77a60a2866d113951cf7b5ceb44f192a96
peppapig13132 <peppapig13132@outlook.com>
2024-10-12 06:38:22 +0200

    Buffered Channels

--- main.go
+++ main.go
@@ -2,24 +2,10 @@ package main
 
 import "fmt"
 
-func sum(s []int, c chan int) {
-	fmt.Println(s)
-	sum := 0
-	for _, v := range s {
-		sum += v
-	}
-	c <- sum	// send sum to c
-
-	fmt.Println(c)
-}
-
 func main() {
-	s := []int{7, 2, 8, -9, 4, 0}
-
-	c := make(chan int)
-	go sum(s[:len(s)/2], c)
-	go sum(s[len(s)/2:], c)
-	x, y := <-c, <-c	// receive from c
-
-	fmt.Println(x, y, x+y)
+	ch := make(chan int, 2)
+	ch <- 1
+	ch <- 2
+	fmt.Println(<-ch)
+	fmt.Println(<-ch)
 }
